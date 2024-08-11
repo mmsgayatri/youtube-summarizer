fffb7416e87dd41c0a8f08b6d7c0d61801ac72ba
mmsgayatri <128353744+mmsgayatri@users.noreply.github.com>
2024-08-11 23:18:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
-
-    import streamlit as st
+import streamlit as st
 import google.generativeai as genai
 from youtube_transcript_api import YouTubeTranscriptApi
 from googletrans import Translator
